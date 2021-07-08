6b1e1494f6d11bfa087153fcb01f1b83b7e02bf0
liaolonghui <1340881930@qq.com>
2021-07-08 17:57:50 +0800

    Graze半成品

--- src/components/Graze.vue
+++ src/components/Graze.vue
@@ -18,21 +18,50 @@
         <span>The Grazing Range is where the herd can come to graze on rare crops.</span>
         <span>Stake your ibALPACA to earn rewards of your choice.</span>
         <span>No risks. High APYs. Happy Alpacas.</span>
+        <!-- graze-type -->
         <div class="graze-type">
-          <div>Live</div>
-          <div>Completed</div>
+          <div @click="changeGrazeType" :class="{'active': grazeType==='Live'}">Live</div>
+          <div @click="changeGrazeType" :class="{'active': grazeType==='Completed'}">Completed</div>
+          <!-- switch -->
+          <div class="switch-box">
+            <switches v-model="stakedOnly" theme="bootstrap" color="success"></switches>
+            <span>Staked only</span>
+          </div>
         </div>
         <img src="../assets/images/grazing-range.svg" alt="grazing-range" class="graze-img">
       </header>
       <!-- body -->
       <article class="graze-body">
-
+        <!-- pc -->
+        <!-- mobile -->
       </article>
     </div>
     <!-- spacer -->
     <div class="spacer-50"></div>
   </div>
 </template>
+
+<script>
+import Switches from 'vue-switches'
+
+export default {
+  components: {
+    Switches
+  },
+  data() {
+    return {
+      grazeType: 'Live',
+      // Staked only
+      stakedOnly: false
+    }
+  },
+  methods: {
+    changeGrazeType(e) {
+      this.grazeType = e.target.innerText
+    }
+  },
+}
+</script>
 
 <style>
 .graze {
@@ -95,5 +124,35 @@
   position: absolute;
   top: 0;
   right: 0;
+}
+/* header中的几个筛选按钮 */
+.graze-type {
+  position: relative;
+  z-index: 66;
+  margin-top: 60px;
+}
+.graze-type>div {
+  display: inline-block;
+  padding: 10px 20px;
+  font-weight: 700;
+  color: #9ca3af;
+  border-radius: 20px;
+  background-color: #fff;
+  cursor: pointer;
+}
+.graze-type>div.active {
+  color: #fff;
+  background-color: #31c77f;
+}
+/* header中的switch */
+.switch-box {
+  margin-left: 30px;
+}
+.switch-box span {
+  display: inline-block;
+  vertical-align: 7px;
+}
+.vue-switcher {
+  margin-right: 10px;
 }
 </style>
