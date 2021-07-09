282e110e1c1b8234f56d210d2c22d8c737310358
liaolonghui <1340881930@qq.com>
2021-07-09 09:51:25 +0800

    简单连接一下钱包，先试一下钱包地址

--- src/views/App.vue
+++ src/views/App.vue
@@ -10,21 +10,26 @@
       </span>
       <!-- btn -->
       <span class="btn-box">
-        <!-- connect-btn -->
-        <div class="btn btn-default hidden-xs">Connect to a wallet</div>
+        <!-- connect-btn pc -->
+        <div @click="connectWallet" class="btn btn-default connect-btn hidden-xs">
+          {{publicAddress ? 'Connected as '+publicAddress : 'Connect to a wallet'}}
+        </div>
         <!-- <button class="set-btn">
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="1em" height="1em" fill="currentColor" aria-hidden="true" focusable="false" class=""><path d="M29.56,19.36a1,1,0,0,0-1.21.07A10.49,10.49,0,0,1,21.51,22,10.17,10.17,0,0,1,11.2,12a9.94,9.94,0,0,1,4.28-8.1,1,1,0,0,0,.36-1.17,1,1,0,0,0-1-.64A14.1,14.1,0,0,0,2,16,14.21,14.21,0,0,0,16.37,30a14.34,14.34,0,0,0,13.57-9.44A1,1,0,0,0,29.56,19.36ZM16.37,28A12.2,12.2,0,0,1,4,16,12,12,0,0,1,11.57,4.89,11.82,11.82,0,0,0,9.2,12,12.17,12.17,0,0,0,21.51,24a12.49,12.49,0,0,0,4.89-1A12.5,12.5,0,0,1,16.37,28Z" data-name="Layer 46"></path></svg>
         </button> -->
-        <button class="set-btn">
+        <!-- Settings -->
+        <button @click="showSetDialog" class="set-btn">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="setting" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M924.8 625.7l-65.5-56c3.1-19 4.7-38.4 4.7-57.8s-1.6-38.8-4.7-57.8l65.5-56a32.03 32.03 0 009.3-35.2l-.9-2.6a443.74 443.74 0 00-79.7-137.9l-1.8-2.1a32.12 32.12 0 00-35.1-9.5l-81.3 28.9c-30-24.6-63.5-44-99.7-57.6l-15.7-85a32.05 32.05 0 00-25.8-25.7l-2.7-.5c-52.1-9.4-106.9-9.4-159 0l-2.7.5a32.05 32.05 0 00-25.8 25.7l-15.8 85.4a351.86 351.86 0 00-99 57.4l-81.9-29.1a32 32 0 00-35.1 9.5l-1.8 2.1a446.02 446.02 0 00-79.7 137.9l-.9 2.6c-4.5 12.5-.8 26.5 9.3 35.2l66.3 56.6c-3.1 18.8-4.6 38-4.6 57.1 0 19.2 1.5 38.4 4.6 57.1L99 625.5a32.03 32.03 0 00-9.3 35.2l.9 2.6c18.1 50.4 44.9 96.9 79.7 137.9l1.8 2.1a32.12 32.12 0 0035.1 9.5l81.9-29.1c29.8 24.5 63.1 43.9 99 57.4l15.8 85.4a32.05 32.05 0 0025.8 25.7l2.7.5a449.4 449.4 0 00159 0l2.7-.5a32.05 32.05 0 0025.8-25.7l15.7-85a350 350 0 0099.7-57.6l81.3 28.9a32 32 0 0035.1-9.5l1.8-2.1c34.8-41.1 61.6-87.5 79.7-137.9l.9-2.6c4.5-12.3.8-26.3-9.3-35zM788.3 465.9c2.5 15.1 3.8 30.6 3.8 46.1s-1.3 31-3.8 46.1l-6.6 40.1 74.7 63.9a370.03 370.03 0 01-42.6 73.6L721 702.8l-31.4 25.8c-23.9 19.6-50.5 35-79.3 45.8l-38.1 14.3-17.9 97a377.5 377.5 0 01-85 0l-17.9-97.2-37.8-14.5c-28.5-10.8-55-26.2-78.7-45.7l-31.4-25.9-93.4 33.2c-17-22.9-31.2-47.6-42.6-73.6l75.5-64.5-6.5-40c-2.4-14.9-3.7-30.3-3.7-45.5 0-15.3 1.2-30.6 3.7-45.5l6.5-40-75.5-64.5c11.3-26.1 25.6-50.7 42.6-73.6l93.4 33.2 31.4-25.9c23.7-19.5 50.2-34.9 78.7-45.7l37.9-14.3 17.9-97.2c28.1-3.2 56.8-3.2 85 0l17.9 97 38.1 14.3c28.7 10.8 55.4 26.2 79.3 45.8l31.4 25.8 92.8-32.9c17 22.9 31.2 47.6 42.6 73.6L781.8 426l6.5 39.9zM512 326c-97.2 0-176 78.8-176 176s78.8 176 176 176 176-78.8 176-176-78.8-176-176-176zm79.2 255.2A111.6 111.6 0 01512 614c-29.9 0-58-11.7-79.2-32.8A111.6 111.6 0 01400 502c0-29.9 11.7-58 32.8-79.2C454 401.6 482.1 390 512 390c29.9 0 58 11.6 79.2 32.8A111.6 111.6 0 01624 502c0 29.9-11.7 58-32.8 79.2z"></path></svg>
         </button>
-        <!-- list-btn -->
+        <!-- list-btn mobile -->
         <button @click="showOrHideLink" class="set-btn hidden-lg hidden-md">
           <svg viewBox="64 64 896 896" focusable="false" data-icon="align-right" width="1em" height="1em" fill="currentColor" aria-hidden="true"><path d="M904 158H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 424H408c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h496c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0 212H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8zm0-424H120c-4.4 0-8 3.6-8 8v56c0 4.4 3.6 8 8 8h784c4.4 0 8-3.6 8-8v-56c0-4.4-3.6-8-8-8z"></path></svg>
         </button>
       </span>
-      <!-- connect-btn -->
-      <div class="btn btn-default hidden-lg hidden-md hidden-sm center-h">Connect to a wallet</div>
+      <!-- connect-btn mobile -->
+      <div @click="connectWallet" class="btn btn-default connect-btn hidden-lg hidden-md hidden-sm center-h">
+        {{publicAddress ? 'Connected as '+publicAddress : 'Connect to a wallet'}}
+      </div>
       <!-- routers -->
       <ul class="routers hidden-md hidden-lg" style="display: none;" ref="routers">
         <router-link to="/lend" tag="li" class="active">Lend</router-link>
@@ -110,10 +115,17 @@
 </template>
 
 <script>
+import Web3 from 'web3'
+
 export default {
   data() {
     return {
-      connectDialogVisibly: false
+      // 控制显示/隐藏Set模态框
+      setDialogVisibly: false,
+      // Slippage tolerance (Set里的)
+      SlippageTolerance: '0.25',
+      // 保存钱包地址
+      publicAddress: ''
     }
   },
   methods: {
@@ -121,9 +133,33 @@ export default {
     showOrHideLink() {
       $(this.$refs.routers).slideToggle()
     },
-    // 用于后续控制模态框的显示隐藏
-    showConnectDialog() {
-      this.connectDialogVisibly = true
+    // 用于控制Set模态框的显示隐藏
+    showSetDialog() {
+      this.setDialogVisibly = true
+    },
+    // 连接钱包
+    connectWallet() {
+      let web3Provider;
+      if (window.ethereum) {
+        web3Provider = window.ethereum;
+        try {
+          // 请求用户授权
+          window.ethereum.enable();
+        } catch (error) {
+          // 用户不授权时
+          console.error("User denied account access")
+        }
+      }
+      const web3js = new Web3(web3Provider);//web3js就是你需要的web3实例
+  
+      const that = this
+      web3js.eth.getAccounts(function (error, result) {
+        if (!error) {
+          //授权成功后result能正常获取到账号了
+          that.publicAddress = result[0]
+          console.log(that.publicAddress)
+        }
+      })
     }
   }
 }
@@ -276,5 +312,11 @@ export default {
   .myapp .app-content {
     margin-left: 0;
   }
+}
+.connect-btn {
+  max-width: 225px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
 }
 </style>
